3662153d55ef400af0fd757443df38abf6628aff
unknown <zahi1328@gmail.com>
2021-12-17 17:29:37 +0500

    'added'

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ toggle_button.addEventListener("click", function (e) {
   nav.classList.toggle("nav-show");
   contact.classList.toggle("button-show");
 });
+
+const li = document.querySelectorAll(".links");
+const sec = document.querySelectorAll("section");
+
+function activeMenu() {
+  let len = sec.length;
+  while (--len && window.scrollY + 97 < sec[len].offsetTop) {}
+  li.forEach((itx) => itx.classList.remove("active-nav"));
+  li[len].classList.add("active-nav");
+}
+activeMenu();
+window.addEventListener("scroll", activeMenu);
